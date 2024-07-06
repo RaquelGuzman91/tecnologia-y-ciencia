4ae1090ff85ff382918f62c5a7fc03290b4c39d8
RaquelGuzman91 <rakelguzman91@gmail.com>
2024-07-06 16:24:50 -0600

    pagina colegio tecnologia y ciencia

---
+++ carrusel.js
@@ -0,0 +1,49 @@
+const btnLeft = document.querySelector(".btn-left");
+const btnRight = document.querySelector(".btn-right");
+const slider = document.querySelector("#slider");
+const sliderSection = document.querySelectorAll(".slider-section");
+
+btnLeft.addEventListener("click", e => moveToLeft())
+btnRight.addEventListener("click", e => moveToRight())
+
+/*codigo slider infinito*/
+setInterval(() => {
+    moveToRight()
+}, 3000);
+
+let operacion = 0;
+    counter = 0,
+    widthImg = 100 / sliderSection.length;
+
+function moveToRight(){
+    if (counter >= sliderSection.length-1) {
+        counter = 0;
+        operacion = 0;
+        slider.style.transform = `translate(-${operacion}%)`;
+/**quita la transition al llegar a la ultima y volver a la primera imagen**/
+        slider.style.transition = "none";
+        return;
+    } 
+    counter++;
+    
+        operacion = operacion + widthImg;
+        slider.style.transform = `translate(-${operacion}%)`;
+        slider.style.transition = "all ease .6s"
+    
+}
+
+function moveToLeft(){
+    counter--;
+    if(counter < 0 ) {
+        counter = sliderSection.length-1;
+        operacion = widthImg * (sliderSection.length-1)
+        slider.style.transform = `translate(-${operacion}%)`;
+/**quita la transition al llegar a la ultima y volver a la primera imagen**/
+        slider.style.transition = "none";
+        return
+    } 
+    operacion = operacion - widthImg;
+        slider.style.transform = `translate(-${operacion}%)`;
+        slider.style.transition = "all ease .6s"
+
+}
